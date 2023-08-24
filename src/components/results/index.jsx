85bd1b61d38a06bcb204cdd79f399036881af53f
emreates2 <emre.ates@trailblu.com>
2023-08-24 19:32:43 +0300

    Sort options

--- src/components/results/index.jsx
+++ src/components/results/index.jsx
@@ -1,41 +1,209 @@
-import React from "react";
-function Results({ resultsOneWay, resultRoundTrip }) {
-  console.log("Results", resultsOneWay);
+import React, { useEffect, useState } from "react";
+function Results({
+  resultsOneWay,
+  resultsReturn,
+  isOneWay,
+  isFormIncomplete,
+  from,
+  to,
+  dateDeparture,
+  dateReturn,
+}) {
+  console.log(isFormIncomplete);
+  console.log(resultsOneWay);
+  console.log(resultsReturn);
+
+  const [sortedResultsOneWay, setSortedResultsOneWay] = useState();
+  const [sortedResultsReturn, setSortedResultsReturn] = useState();
+  const [sortKey, setSortKey] = useState(null);
+  const [ascending, setAscending] = useState(true);
+
+  useEffect(() => {
+    setSortedResultsOneWay(resultsOneWay);
+    setSortedResultsReturn(resultsReturn);
+  }, [resultsOneWay, resultsReturn]);
+
+  const formatTo24Hour = (timeString) => {
+    const [hours, minutes] = timeString.split(".");
+    let totalMinutes = parseInt(hours) * 60 + parseInt(minutes);
+
+    if (hours.startsWith("0")) {
+      totalMinutes += 24 * 60;
+    }
+
+    return totalMinutes;
+  };
+
+  const handleSort = (key, dataType) => {
+    if (key === sortKey) {
+      setAscending(!ascending);
+    } else {
+      setSortKey(key);
+      setAscending(true);
+    }
+
+    if (dataType === "oneway") {
+      const sorted = [...sortedResultsOneWay].sort((a, b) => {
+        const aValue = key === "arrivaltime" ? formatTo24Hour(a[key]) : a[key];
+        const bValue = key === "arrivaltime" ? formatTo24Hour(b[key]) : b[key];
+
+        if (aValue < bValue) return ascending ? -1 : 1;
+        if (aValue > bValue) return ascending ? 1 : -1;
+        return 0;
+      });
+
+      setSortedResultsOneWay(sorted);
+    } else {
+      const sorted = [...sortedResultsReturn].sort((a, b) => {
+        const aValue = key === "arrivaltime" ? formatTo24Hour(a[key]) : a[key];
+        const bValue = key === "arrivaltime" ? formatTo24Hour(b[key]) : b[key];
+
+        if (aValue < bValue) return ascending ? -1 : 1;
+        if (aValue > bValue) return ascending ? 1 : -1;
+        return 0;
+      });
+
+      setSortedResultsReturn(sorted);
+    }
+  };
+
   return (
-    <div className='flex w-1/2 mx-auto mt-10 items-center justify-between'>
-      <div className='bg-red-400 w-1/2'>
-        <h1 className='text-3xl text-center p-5 border-black border-b-2'>
-          {resultsOneWay[0]?.from} - {resultsOneWay[0]?.to}
-        </h1>
-        {resultsOneWay.map((result, index) => {
-          return (
-            <div key={index} className='border-black border-b-2'>
-              <h1>Company: {result.company}</h1>
-              <h1>Departure Date: {result.departuredate}</h1>
-              <h1>Departure Time: {result.departuretime}</h1>
-              <h1>Price: {result.price} $</h1>
-              <h1>Time: {result.time}</h1>
-            </div>
-          );
-        })}
+    <div className='flex w-1/2 h-screen mx-auto mt-10 items-center justify-between rounded-xl overflow-hidden'>
+      <div className={`bg-red-400 h-full ${isOneWay ? "w-2/3 m-auto" : "w-1/2"}`}>
+        {!isFormIncomplete ? (
+          <h1 className='text-3xl text-center p-5 border-black border-b-2'>
+            {from} - {to}
+            <p>{dateDeparture}</p>
+          </h1>
+        ) : (
+          <p className='text-center mt-5 text-3xl'>Please Fill the Blanks</p>
+        )}
+
+        {!resultsOneWay.length > 0 && !isFormIncomplete ? (
+          <p className='text-center mt-5 text-3xl'>There is no result</p>
+        ) : (
+          <table className='w-full'>
+            {!isFormIncomplete && (
+              <tr className='border-b-4 border-blue-300 h-10'>
+                <th
+                  className='cursor-pointer select-none w-1/5 transition-all hover:bg-red-800'
+                  onClick={() => handleSort("company", "oneway")}
+                >
+                  Company
+                </th>
+                <th
+                  className='cursor-pointer select-none w-1/5 transition-all hover:bg-red-800'
+                  onClick={() => handleSort("departuretime", "oneway")}
+                >
+                  Departure
+                </th>
+                <th
+                  className='cursor-pointer select-none w-1/5 transition-all hover:bg-red-800'
+                  onClick={() => handleSort("time", "oneway")}
+                >
+                  Duration
+                </th>
+                <th
+                  className='cursor-pointer select-none w-1/5 transition-all hover:bg-red-800'
+                  onClick={() => handleSort("arrivaltime", "oneway")}
+                >
+                  Arrival
+                </th>
+                <th
+                  className='cursor-pointer select-none w-1/5 transition-all hover:bg-red-800'
+                  onClick={() => handleSort("price", "oneway")}
+                >
+                  Price
+                </th>
+              </tr>
+            )}
+            {!isFormIncomplete &&
+              sortedResultsOneWay?.map((result, index) => {
+                return (
+                  <tr
+                    key={index}
+                    className='select-none h-20 text-center border-b-2 border-black hover:bg-gray-500 transition-colors'
+                  >
+                    <td>{result.company}</td>
+                    <td>{result.departuretime}</td>
+                    <td>{result.time}</td>
+                    <td>{result?.arrivaltime}</td>
+                    <td>{result.price} $</td>
+                  </tr>
+                );
+              })}
+          </table>
+        )}
       </div>
 
-      <div className='bg-green-400 w-1/2'>
-        <h1 className='text-3xl text-center p-5 border-black border-b-2'>
-          {resultsOneWay[0]?.to} - {resultsOneWay[0]?.from}
-        </h1>
-        {resultsOneWay.map((result, index) => {
-          return (
-            <div key={index} className='border-black border-b-2'>
-              <h1>Company: {result.company}</h1>
-              <h1>Departure Date: {result.departuredate}</h1>
-              <h1>Departure Time: {result.departuretime}</h1>
-              <h1>Price: {result.price} $</h1>
-              <h1>Time: {result.time}</h1>
-            </div>
-          );
-        })}
-      </div>
+      {!isOneWay && (
+        <div className='bg-green-400 h-full w-1/2'>
+          {!isFormIncomplete ? (
+            <h1 className='text-3xl text-center p-5 border-black border-b-2'>
+              {to} - {from}
+              <p>{dateReturn}</p>
+            </h1>
+          ) : (
+            <p className='text-center mt-5 text-3xl'>Please Fill the Blanks</p>
+          )}
+
+          {!resultsReturn.length > 0 && !isFormIncomplete ? (
+            <p className='text-center mt-5 text-3xl'>There is no result</p>
+          ) : (
+            <table className='w-full'>
+              {!isFormIncomplete && (
+                <tr className='border-b-4 border-blue-300 h-10'>
+                  <th
+                    className='cursor-pointer select-none w-1/5 transition-all hover:bg-red-800'
+                    onClick={() => handleSort("company")}
+                  >
+                    Company
+                  </th>
+                  <th
+                    className='cursor-pointer select-none w-1/5 transition-all hover:bg-red-800'
+                    onClick={() => handleSort("departuretime")}
+                  >
+                    Departure
+                  </th>
+                  <th
+                    className='cursor-pointer select-none w-1/5 transition-all hover:bg-red-800'
+                    onClick={() => handleSort("time")}
+                  >
+                    Duration
+                  </th>
+                  <th
+                    className='cursor-pointer select-none w-1/5 transition-all hover:bg-red-800'
+                    onClick={() => handleSort("arrivaltime")}
+                  >
+                    Arrival
+                  </th>
+                  <th
+                    className='cursor-pointer select-none w-1/5 transition-all hover:bg-red-800'
+                    onClick={() => handleSort("price")}
+                  >
+                    Price
+                  </th>
+                </tr>
+              )}
+              {!isFormIncomplete &&
+                sortedResultsReturn.map((result, index) => {
+                  return (
+                    <tr
+                      key={index}
+                      className='h-20 text-center border-b-2 border-black'
+                    >
+                      <td>{result.company}</td>
+                      <td>{result.departuretime}</td>
+                      <td>{result.time}</td>
+                      <td>{result?.arrivaltime}</td>
+                      <td>{result.price} $</td>
+                    </tr>
+                  );
+                })}
+            </table>
+          )}
+        </div>
+      )}
     </div>
   );
 }
--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -12,9 +12,6 @@ function formatDate(inputDate) {
   return formattedDate;
 }
 
-// Notes: 
-//  - From şehrini seçince To şehrine veri geri gelmiyor
-
 function SearchUi() {
   const [showOptionsFrom, setShowOptionsFrom] = useState(false);
   const [showOptionsTo, setShowOptionsTo] = useState(false);
@@ -34,15 +31,6 @@ function SearchUi() {
     setFromOptions(Cities.results);
     setToOptions(Cities.results);
   }, []);
-
-  useEffect(() => {
-    if (formData.from) {
-      const updatedToOptions = toOptions?.filter(
-        (option) => option !== formData.from
-      );
-      setToOptions(updatedToOptions);
-    }
-  }, [formData.from]);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -130,25 +118,41 @@ function SearchUi() {
   };
 
   console.log(formData);
-  const date = formatDate(formData.departure);
-  console.log(date);
+  const dateDeparture = formatDate(formData.departure);
+  const dateReturn = formatDate(formData.return);
 
-  const searchData = Flights.results.filter((item) => {
+  const searchDataDeparture = Flights.results.filter((item) => {
     return (
       item.from === formData.from &&
       item.to === formData.to &&
-      item.departuredate === date
+      item.departuredate === dateDeparture
     );
   });
 
-  console.log("Search Data", searchData);
+  const searchDataReturn = Flights.results.filter((item) => {
+    return (
+      item.from === formData.to &&
+      item.to === formData.from &&
+      item.departuredate === dateReturn
+    );
+  });
+
+  // const isFormIncomplete = Object.values(formData).some((value) => value === "");
+
+  const isFormIncomplete = isOneWay
+    ? ["from", "to", "departure"].some((field) => formData[field] === "")
+    : Object.values(formData).some((value) => value === "");
+
+  console.log(isFormIncomplete);
+  console.log(isOneWay);
 
   return (
     <div>
       <section className='bg-yellow-300 rounded-xl text-black w-1/3 mx-auto mt-10 p-14'>
         <TripChoice />
-
+        
         <div className='flex gap-x-4 w-full justify-between'>
+          {/* From Cities */}
           <div className='flex gap-x-1 items-center w-full'>
             <label htmlFor='from'>From</label>
             <div className='relative'>
@@ -166,23 +170,25 @@ function SearchUi() {
               />
               {showOptionsFrom && (
                 <ul className='absolute top-full left-0 w-full z-10 text-lg'>
-                  <option
-                    onClick={() =>
+                  <li
+                    onClick={() => {
                       setFormData((prevData) => ({
                         ...prevData,
-                        from: "Select",
-                      }))
-                    }
+                        from: "",
+                      }));
+                      setShowOptionsFrom(false);
+                    }}
                     className='border-b pl-2 rounded-md bg-white border-black cursor-pointer hover:bg-yellow-200 transition-all'
                   >
                     Select
-                  </option>
+                  </li>
                   {fromOptions
                     .filter((option) =>
                       option.toLowerCase().includes(formData.from.toLowerCase())
                     )
                     .map((option, index) => (
-                      <li
+                      <option
+                        disabled={formData.to === option}
                         key={index}
                         onClick={() => {
                           setFormData((prevData) => ({
@@ -191,15 +197,19 @@ function SearchUi() {
                           }));
                           setShowOptionsFrom(false);
                         }}
-                        className='border-b pl-2 rounded-md bg-white border-black cursor-pointer hover:bg-yellow-200 transition-all'
+                        className={`border-b pl-2 rounded-md bg-white border-black cursor-pointer hover:bg-yellow-200 transition-all ${
+                          formData.to === option ? "text-red-400" : ""
+                        }`}
                       >
                         {option}
-                      </li>
+                      </option>
                     ))}
                 </ul>
               )}
             </div>
           </div>
+
+          {/* To Cities */}
           <div className='flex gap-x-1 items-center relative'>
             <label htmlFor='to'>To</label>
             <div className='relative'>
@@ -216,24 +226,25 @@ function SearchUi() {
               />
               {showOptionsTo && (
                 <ul className='absolute top-full left-0 w-full z-10 text-lg'>
-                  <option
+                  <li
                     onClick={() => {
                       setFormData((prevData) => ({
                         ...prevData,
-                        to: "Select",
+                        to: "",
                       }));
                       setShowOptionsTo(false);
                     }}
                     className='border-b pl-2 rounded-md bg-white border-black cursor-pointer hover:bg-yellow-200 transition-all'
                   >
                     Select
-                  </option>
+                  </li>
                   {toOptions
                     .filter((option) =>
                       option.toLowerCase().includes(formData.to.toLowerCase())
                     )
                     .map((option, index) => (
                       <option
+                        disabled={formData.from === option}
                         key={index}
                         onClick={() => {
                           setFormData((prevData) => ({
@@ -242,7 +253,9 @@ function SearchUi() {
                           }));
                           setShowOptionsTo(false);
                         }}
-                        className='border-b pl-2 rounded-md bg-white border-black cursor-pointer hover:bg-yellow-200 transition-all'
+                        className={`border-b pl-2 rounded-md bg-white border-black cursor-pointer hover:bg-yellow-200 transition-all ${
+                          formData.from === option ? "text-red-400" : ""
+                        }`}
                       >
                         {option}
                       </option>
@@ -255,7 +268,16 @@ function SearchUi() {
 
         <DatePick />
       </section>
-      <Results resultsOneWay={searchData} />
+      <Results
+        resultsOneWay={searchDataDeparture}
+        resultsReturn={searchDataReturn}
+        isOneWay={isOneWay}
+        isFormIncomplete={isFormIncomplete}
+        from={formData.from}
+        to={formData.to}
+        dateDeparture={dateDeparture}
+        dateReturn={dateReturn}
+      />
     </div>
   );
 }
