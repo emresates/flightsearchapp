f4fdd09452b6130076b8c7fbbb81ae741c45d032
emreates2 <emre.ates@trailblu.com>
2023-08-25 17:36:41 +0300

    loaders added

---
+++ src/components/loading/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export function Loader1() {
+  return <div className='w-full h-full custom-loader'></div>;
+}
+
+export function Loader2() {
+  return <div className='w-full h-full custom-loader2'></div>;
+}
